7830da85a467e475cb9f47d128c65a4086c9b989
Igor Besel <besel.igor@gmail.com>
2022-02-20 19:06:26 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'until-its-done',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
